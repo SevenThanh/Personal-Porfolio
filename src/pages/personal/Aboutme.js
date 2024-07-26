7d854fb1174fc3f3607d4c3185fa4deea5a85d9d
Johan Nguyen <thaithanhace@gmail.com>
2024-07-26 18:51:38 -0500

    done

--- src/pages/personal/Aboutme.js
+++ src/pages/personal/Aboutme.js
@@ -15,9 +15,9 @@ function Aboutme() {
             <ul>
               <li>🤔 Born in a small, rural town in Vietnam, when I was 13, mom asked me if I wanted to come to America with her. Guess what I said?</li>
               <li>🏋 Every day around 4 pm, you'll find me either at the gym or on a basketball court ( not 6ft and not even good T^T).</li>
-              <li>🎶 I can't live without music, though, I don't have a specific type; if it's good, it's good.</li>
-              <li>😗 I'm a very friendly and chill person, so don't hesitate to reach out. I’m always down for whatever.</li>
-              <li>🌍 I love traveling to different places and experiencing new things. Life is too short, so keep an open mind and don't waste it</li>
+              <li>🎶 Can't live without music, though, I don't have a specific type; if it's good, it's good.</li>
+              <li>😗 Very friendly and chill person, so don't hesitate to reach out. I’m always down for whatever.</li>
+              <li>🌍 Love traveling to different places and experiencing new things. Life is too short, keep an open mind so you don't waste it.</li>
               <li>💙 Whoever you're reading this, remmeber to always be kind and love yourself</li>
             </ul>
           </div>
